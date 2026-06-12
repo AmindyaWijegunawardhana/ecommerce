8d38327727593ab937ba43bc0b0e6172c230236d
NC MOBILE <amindyanw911@gmail.com>
2026-06-12 17:51:21 +0530

    fix: support CLOUDINARY_NAME environment variable on production Render

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,14 +1,16 @@
 const cloudinary = require('cloudinary').v2;
 
+const cloudName = process.env.CLOUDINARY_CLOUD_NAME || process.env.CLOUDINARY_NAME;
+
 const isCloudinaryConfigured = !!(
-  process.env.CLOUDINARY_CLOUD_NAME &&
+  cloudName &&
   process.env.CLOUDINARY_API_KEY &&
   process.env.CLOUDINARY_API_SECRET
 );
 
 if (isCloudinaryConfigured) {
   cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_name: cloudName,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
   });
